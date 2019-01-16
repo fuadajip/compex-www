2000d16ea4d0f38b50060b1083c14cd87b70e4d4
fuadajip <fuadajipratomo@gmail.com>
2019-01-16 19:20:31 +0700

    [FIX] main package json and remove intial files from cra

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Alert from './Alert';
+import Badge from './Badge';
+import {Breadcrumb, BreadcrumbItem} from './Breadcrumb';
+import Button from './Button';
+import Card from './Card';
+import Image from './Image';
+
+export {Alert, Badge, Breadcrumb, BreadcrumbItem, Button, Card, Image};
